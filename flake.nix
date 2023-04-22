a1e21edd0bdb3c9b7f4223422a8f72948933f67d
Ingo Ruhnke <grumbel@gmail.com>
2023-04-23 01:26:04 +0200

    Initial checkin

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    xpenguins_src.url = "http://xpenguins.seul.org/xpenguins-2.2.tar.gz";
+    xpenguins_src.flake = false;
+  };
+
+  outputs = { self, nixpkgs, flake-utils, xpenguins_src }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages = rec {
+          default = xpenguins;
+
+          xpenguins = pkgs.stdenv.mkDerivation rec {
+            pname = "xpenguins";
+            version = "2.2";
+
+            src = xpenguins_src;
+
+            patches = [
+              ./fix-snprintf-error.diff
+            ];
+
+            buildInputs = with pkgs; [
+              xorg.libX11
+              xorg.libXpm
+              xorg.libXt
+              xorg.libXext
+            ];
+          };
+        };
+      }
+    );
+}
