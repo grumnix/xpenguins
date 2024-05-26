dd6da96dd0e33afc492063ee02e3dbe45d2515f4
Ingo Ruhnke <grumbel@gmail.com>
2024-05-26 17:55:25 +0200

    Update to nixos-24.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     xpenguins_src.url = "http://xpenguins.seul.org/xpenguins-2.2.tar.gz";
